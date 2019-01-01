d62a9f99792cb9fb4ceb7b39bd7a859dae6a064e
Zeling Feng <zeling_feng@brown.edu>
2019-01-01 00:00:26 -0800

    fixed a bug if a ^D is entered in the loop

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@
 #include <readline/history.h>
 
 static char *line_read = nullptr;
-std::string rl_gets() {
+void rl_gets() {
     if (line_read) {
         ::free(line_read);
         line_read = nullptr;
@@ -25,30 +25,32 @@ std::string rl_gets() {
 
     if (line_read && *line_read)
         add_history(line_read);
-
-    return { line_read };
 }
 
 int main() {
     using namespace prolog0;
 
     while (true) {
-        std::istringstream iss(rl_gets());
-        scanner sc(iss);
-        parser p(sc);
-        codegen cg;
-        if (sc.peek() == token::type::QMDASH) {
-            /* its a query */
-            auto qry = p.parse_query();
-            cg.compile_query(qry.get());
-            cg.print_to_stream(std::cout);
+        rl_gets();
+        if (line_read) {
+            std::istringstream iss(line_read);
+            scanner sc(iss);
+            parser p(sc);
+            codegen cg;
+            if (sc.peek() == token::type::QMDASH) {
+                /* its a query */
+                auto qry = p.parse_query();
+                cg.compile_query(qry.get());
+                cg.print_to_stream(std::cout);
+            } else {
+                /* its not a query */
+                auto prg = p.parse_program();
+                cg.compile_program(prg.get());
+                cg.print_to_stream(std::cout);
+            }
         } else {
-            /* its not a query */
-            auto prg = p.parse_program();
-            cg.compile_program(prg.get());
-            cg.print_to_stream(std::cout);
+            break;
         }
-
     }
 
     return 0;
